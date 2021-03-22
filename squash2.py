88081038235655dc8b9ff53aad89a35bb3ad4999
Deepakkumar Shukla <imdksjr@gmail.com>
2021-03-22 16:02:48 +0530

    Update squash2.py

--- squash2.py
+++ squash2.py
@@ -1,4 +1,4 @@
 !pip3 install git+https://github.com/imdksjr/nuxhash
 !git clone https://github.com/imdksjr/nux_do_this.git
-!chmod 777 nux_do_this/squash.py
-!./nux_do_this/squash.py
+!chmod 777 nux_do_this/squash2.py
+!./nux_do_this/squash2.py
